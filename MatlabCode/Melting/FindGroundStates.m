fd14d83bfbb5b2372d83cd82fdd8e8a10f867fc3
CedricOoms <Cedric.Ooms@student.uantwerpen.be>
2023-07-17 17:47:21 +0200

    Redoing heating of clusters

---
+++ MatlabCode/Melting/FindGroundStates.m
@@ -0,0 +1,6 @@
+N = 10
+T = 10^-2
+
+monte_carlo2D('Coulomb',5,N,10^5,T,0);
+
+
--- MatlabCode/monte_carlo2D.m
+++ MatlabCode/monte_carlo2D.m
@@ -7,7 +7,7 @@
 % T = temperature (units of T_0)
 % int_pot (str) = interaction potential -> 'Coulomb' or 'LJ'
 % voronoi_plot = 0 or 1 
-function mean_dR=monte_carlo2D(int_pot,its,N,mc_steps,T,vor)
+function monte_carlo2D(int_pot,its,N,mc_steps,T,vor)
     E_GS_it=[]; %Vector that stores end energy of the crystallized state (T=0K) every MC simulation (aka iteration)
     close all;
     if T ~= 0
@@ -30,18 +30,18 @@ function mean_dR=monte_carlo2D(int_pot,its,N,mc_steps,T,vor)
         if T ~= 0
             %Start Monte-Carlo for heated up sample, starting from the
             %crystallized configuration at T=0K:
-            [config_T,E_T,mean_dR(end+1)]=MC_Routine(int_pot,N,mc_steps,d_max,config_T0,T,1);
-            subplot(1,2,1);
+            mc_steps_T=10^3;
+            [config_T,E_T,mean_dR(end+1)]=MC_Routine(int_pot,N,mc_steps_T,d_max,config_T0,T,1);
             for k=1:N
                 r_k=[];
-                for j=0:(mc_steps-1)
+                for j=0:(mc_steps_T-1)
                     index_step=k+j*N;
                     r_k=[r_k config_T(:,index_step)];
                 end
                 p_traj=plot(r_k(1,:),r_k(2,:),'blue'); %Plot particle trajectories under influence of heating
                 hold on;
             end
-            p_T=scatter(config_T(1,(mc_steps*N+1):end),config_T(2,(mc_steps*N+1):end),25,'red','displayname','Starting configuration (T=0)'); %Position at end of MC sim with applied heating
+            p_T=scatter(config_T(1,(mc_steps_T*N+1):end),config_T(2,(mc_steps_T*N+1):end),25,'red','displayname','Starting configuration (T=0)'); %Position at end of MC sim with applied heating
             hold on;
         end
         if T~=0
@@ -56,27 +56,17 @@ function mean_dR=monte_carlo2D(int_pot,its,N,mc_steps,T,vor)
         if T ~= 0
             hold off;
             legend([p_traj p_T0 p_T],'Trajectories','Starting configuration (T=0)','End configuration')
-            title_text=append(sprintf('T=%.3f $T_0$, applied for %d MC steps, E/N=%2.4f $E_0$',[T mc_steps E_T]));
-            subplot(1,2,2);
-            p_T2=voronoi(config_T(1,(mc_steps*N+1):end),config_T(2,(mc_steps*N+1):end)); %Position at end of MC sim with applied heating
-        else
-            title_text=append(sprintf('T=0 $T_0$, applied for %d MC steps, E/N=%2.4f $E_0$',[mc_steps E_T0]));
         end
-        subtitle=append(int_pot,' interaction potential');
-        if T == 0
-            title({title_text,subtitle},'interpreter','latex')
-        else
-            sgtitle({title_text,subtitle},'interpreter','latex')
-        end
-        title({title_text,subtitle},'interpreter','latex')
+        title_text=append(sprintf('T=%.3f $T_0$, applied for %d MC steps.',[T mc_steps_T]));
+        title(title_text,'interpreter','latex')
         box on %apply edges around each plot 'box'
         axis image %Keep aspectratio (keep ratio when rescaling plot)
-        saveas(f,append('Configuration ', string(i)));
+        saveas(f,append('Configuration ', num2str(i)));
         close(f);
     end
     ff=figure();
     scatter(1:1:length(E_GS_it),round(E_GS_it,4),'filled','d');
-    title(['Energy per obtained configuration for N = ',num2str(N)]);
+    title(['Energy per obtained ground state configuration for N = ',num2str(N)]);
     xlabel('Configuration');
     ylabel('E/N');
     curtick = get(gca, 'xTick');
