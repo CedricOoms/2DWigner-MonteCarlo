f975d354c14ca7f5e2f90acbd1c330c8a6989540
CedricOoms <Cedric.Ooms@student.uantwerpen.be>
2023-07-23 16:47:38 +0200

    Changed code for heating

--- MatlabCode/Monte_Carlo_Heating.m
+++ MatlabCode/Monte_Carlo_Heating.m
@@ -24,28 +24,31 @@ function Monte_Carlo_Heating(its,N,mc_steps,T_start,T_fin)
         %Plot for result:
         f=figure();
         %Start Monte-Carlo for heated up sample, starting from the
-        %crystallized configuration at T=0K:
+        %crystallized configuration at T=0K.
+        %Temperature is incrementally increased and after each increment the system is allowed to equilibrialize for 10^5 MC steps:
         config_T = config_T0;
         T = T_start;
         while T <= T_fin
             %Equilibrialize at the new temperature for 10^5 MC steps:
             T = T + dT;
             mc_steps_eq = 10^5;
-            [config_T_eq,E_T,mean_dR(end+1)]=MC_Routine('Coulomb',N,mc_steps_eq,d_max,config_T,T,1);
-    
-            %
-            mc_steps_T=10^3;
-            [config_T,E_T,mean_dR(end+1)]=MC_Routine(int_pot,N,mc_steps_T,d_max,config_T_eq,T,1);
-            for k=1:N
-                r_k=[];
-                for j=0:(mc_steps_T-1)
-                    index_step=k+j*N;
-                    r_k=[r_k config_T(:,index_step)];
-                end
-                p_traj=plot(r_k(1,:),r_k(2,:),'blue'); %Plot particle trajectories under influence of heating
-                hold on;
-            end
+            [config_T_eq,E_T,~]=MC_Routine('Coulomb',N,mc_steps_eq,d_max,config_T,T,1);
         end
+        
+        %T_fin has been reached and the system has equilibrialized at that temperature.
+        %Particle trajectories are plotted for 10^3 MC steps at T = T_fin:
+        mc_steps_T=10^3;
+        [config_T,E_T,~]=MC_Routine(int_pot,N,mc_steps_T,d_max,config_T_eq,T,1);
+	for k=1:N
+	    r_k=[];
+	    for j=0:(mc_steps_T-1)
+	        index_step=k+j*N;
+		r_k=[r_k config_T(:,index_step)];
+	    end
+	    p_traj=plot(r_k(1,:),r_k(2,:),'blue'); %Particle trajectories
+	    hold on;
+	end
+	
         p_T=scatter(config_T(1,(mc_steps_T*N+1):end),config_T(2,(mc_steps_T*N+1):end),25,'red','displayname','Starting configuration (T=0)'); %Position at end of MC sim with applied heating
         hold on;
         p_T0=scatter(config_T0(1,:),config_T0(2,:),25,'filled','black','displayname','End configuration'); %Starting config at T=0K (before heating) 
